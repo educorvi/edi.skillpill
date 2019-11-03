669682f4f3b0281b7c46a42654e6ba8eb7b2fc7d
lwalther <lwalther@novareto.de>
2019-11-03 18:31:30 +0100

    validation finished

--- src/edi/skillpill/views/skill_view.py
+++ src/edi/skillpill/views/skill_view.py
@@ -2,6 +2,7 @@
 import random
 from edi.skillpill import _
 from Products.Five.browser import BrowserView
+from collective.beaker.interfaces import ISession
 
 
 # from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
@@ -11,16 +12,26 @@ class SkillView(BrowserView):
 
     def get_quiz(self):
         quiz = {}
+        quiz['uid'] = self.context.UID()
         quiz['frage'] = self.context.quizfrage
         antworten = []
         for i in self.context.antworten:
             antworten.append((self.context.antworten.index(i), i.get('antwort')))
-        random.shuffle(antworten)
+        #random.shuffle(antworten)
         quiz['antworten'] = antworten
         quiz['image'] = ''
         if self.context.quizimage:
             quiz['image'] = "%s/@@images/quizimage" % self.context.absolute_url()
         return quiz
+
+    def get_validated(self):
+        validated = {}
+        uid = self.context.UID()
+        session = ISession(self.request)
+        if uid in session:
+            validated = session[uid]['validated']
+        return validated
+
 
     def get_action(self):
         return self.context.absolute_url() + '/validate'
--- src/edi/skillpill/views/validate.py
+++ src/edi/skillpill/views/validate.py
@@ -1,11 +1,35 @@
 # -*- coding: utf-8 -*-
 
 from Products.Five.browser import BrowserView
+from collective.beaker.interfaces import ISession
 
 
 class Validate(BrowserView):
+
     def __call__(self):
-        template = '''<li class="heading" i18n:translate="">
-          Sample View
-        </li>'''
-        return template
+        antworten = self.context.antworten
+        uid = self.context.UID()
+        yours = int(self.request.get(uid))
+        success = False
+        validated = []
+        for i in antworten:
+            index = antworten.index(i)
+            i['btnclass'] = 'btn btn-secondary btn-sm btn-block'
+            if i.get('bewertung') in ['richtig', 'success']:
+                i['btnclass'] = 'btn btn-success btn-sm btn-block'
+            if index == yours:
+                i['yours'] = True
+                if i.get('bewertung') in ['richtig', 'success']:
+                    success = True
+                else:
+                    i['btnclass'] = 'btn btn-danger btn-sm btn-block'
+            else:
+                i['yours'] = False
+            validated.append(i)
+        cookie = {'success':success, 'validated':validated}
+        session = ISession(self.request)
+        if uid not in session:
+            session[uid] = cookie
+            session.save()
+        url = "%s%s" % (self.context.absolute_url(), '#ergebnis')
+        return self.request.response.redirect(url)
